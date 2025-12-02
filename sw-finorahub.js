df024ecfe3e4e51aeb4f4e51badc994c225be419
Moh Arif Rifai <moharifrifai@gmail.com>
2025-12-02 12:46:27 +0700

    Update zoneId in sw-finorahub.js

--- sw-finorahub.js
+++ sw-finorahub.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10265258
+    "zoneId": 10265285
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
